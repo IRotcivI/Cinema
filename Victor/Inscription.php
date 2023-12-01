6295dfaec9ae7b56f4ecc5bf59ee6efa183ea972
FAYE <fayedaour@outlook.fr>
2023-12-01 14:32:29 +0100

    Fix : Ajout de nouvelle variable (....)

--- Victor/Inscription.php
+++ Victor/Inscription.php
@@ -7,15 +7,15 @@ $prenom = $_POST['prenom'];
 $email = $_POST['email'];
 $mdp = $_POST['mdp'];
 $age = $_POST['age'];
-$tel = $_POST['tel'];
-$rue = $_POST['rue'];
+$tel = $_POST['telephone'];
+$rue = $_POST['adresse'];
 $ville = $_POST['ville'];
-$cp = $_POST['cp'];
+$cp = $_POST['code_postal'];
 $bdd = new PDO('mysql:host=localhost:3306;dbname=vsa_cinema;charset=utf8', 'root', '');
 
 //CODE
 
-$requete = $bdd->prepare('INSERT INTO utilisateur (nom,prenom,age,email,tel,rue,ville,cp) VALUES (:nom,:prenom,:age,:métiers,:pays,:email,:mdp)');
+$requete = $bdd->prepare('INSERT INTO utilisateur (nom,prenom,age,email,tel,rue,ville,cp) VALUES (:nom,:prenom,:age,:email,:tel,:rue,:ville,:cp)');
 $requete->execute(array(
     'nom' => $nom,
     'prenom' => $prenom,
@@ -23,7 +23,9 @@ $requete->execute(array(
     'mdp' => $mdp,
     'age' => $age,
     'tel' => $tel,
-    'métiers' => $metiers,
+    'rue' => $rue,
+    'ville' => $ville,
+    'cp' => $cp,
 ));
 echo "Votre compte a été crée, veuilez revenir a la page de connexion !";
 
