8ce44ee8b84158238e6d13f6515c2e9c21598954
FAYE <fayedaour@outlook.fr>
2023-12-26 18:17:53 +0100

    Ajout : Variable "$_SESSION" a été ajouté.

--- Page_Cinema/CONNEXION_INSCRIPTION/Page_connexion/Connection.php
+++ Page_Cinema/CONNEXION_INSCRIPTION/Page_connexion/Connection.php
@@ -2,8 +2,8 @@
 session_start();
 
 //VARIABLES
-$email = $_POST['email'];
-$mdp = $_POST['mdp'];
+$_SESSION['email'] = $email = $_POST['email'];
+$_SESSION ['mdp'] = $mdp = $_POST['mdp'];
 $bdd = new PDO('mysql:host=localhost:3306;dbname=vsa_cinema;charset=utf8', 'root', '');
 
 //CODE
@@ -12,9 +12,8 @@ $requete->execute(array(
     'email'=>$email,
     'mdp'=>$mdp,
 ));
-if ($requete->rowCount() > 0){
-
-    echo 'MENU';
+if ($requete->rowCount() > 0 && isset($_SESSION['email']) && isset($_SESSION['mdp'])){
+    echo 'Vous etes connecter';
 }
 else{
     echo 'ERREUR DE CONNECTION';
