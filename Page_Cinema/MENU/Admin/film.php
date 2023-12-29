c3405fcf2d7fd793ae341af1f8cf7f4ebbc0fdf3
FAYE <fayedaour@outlook.fr>
2023-12-30 00:26:06 +0100

    Mise a jour : Séparation du code php (envoie de donnée) au code html

---
+++ Page_Cinema/MENU/Admin/film.php
@@ -0,0 +1,17 @@
+<?php
+//VARIABLES
+$bdd = new PDO('mysql:host=localhost:3306;dbname=vsa_cinema;charset=utf8', 'root', '');
+$nom = $_POST['nom'];
+$desc = $_POST['desc'];
+$affiche = $_POST['aff'];
+$rea = $_POST['rea'];
+
+
+//CODE
+$requete = $bdd -> prepare('INSERT INTO film (nom,description,affiche,realisateur) VALUES (:nom, :description, :affiche, :realisateur)');
+$requete -> execute(array(
+    'nom' => $nom,
+    'description' => $desc,
+    'affiche' => $affiche,
+    'realisateur' => $rea,
+));
