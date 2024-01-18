106b9e96cf40910dba7b0421b3336f28ade395ad
asad.ahad <r.asad@lprs.fr>
2024-01-18 02:11:38 +0100

    Liaison avec la nouvelle page reserver

--- Page_Cinema/CONNEXION_INSCRIPTION/Page_connexion/Connection.php
+++ Page_Cinema/CONNEXION_INSCRIPTION/Page_connexion/Connection.php
@@ -2,22 +2,24 @@
 session_start();
 
 //VARIABLES
-$_SESSION['email'] = $email = $_POST['email'];
-$_SESSION ['mdp'] = $mdp = $_POST['mdp'];
+$email = $_POST['email'];
+$mdp = $_POST['mdp'];
 $bdd = new PDO('mysql:host=localhost:3306;dbname=vsa_cinema;charset=utf8', 'root', '');
 
 //CODE
 $requete = $bdd->prepare('SELECT * FROM utilisateur WHERE email = :email AND mdp = :mdp');
 $requete->execute(array(
-    'email'=>$email,
-    'mdp'=>$mdp,
+    'email' => $email,
+    'mdp' => $mdp,
 ));
-if ($requete->rowCount() > 0 && isset($_SESSION['email']) && isset($_SESSION['mdp'])){
-    echo 'Vous etes connecter';
-}
-else{
+
+if ($requete->rowCount() > 0) {
+    header('Location: test.html');
+    exit();
+} else {
     echo 'ERREUR DE CONNECTION';
-    echo '<form action="Cinema.html">
-    <input type="submit" value="Page Connection"/><br>
-    </form>';
+    echo '<form action="Cinema.html" method="post">
+            <input type="submit" value="Page Connection"/><br>
+          </form>';
 }
+?>
