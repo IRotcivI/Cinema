46a8e4b87254525306ab9ac4011a363311eb99f4
ASAD <r.asad@lprs.fr>
2024-01-19 09:30:43 +0100

    CORRECTION PAGE RESERVER

--- Page_Cinema/MENU/mm/res.php
+++ Page_Cinema/MENU/mm/res.php
@@ -6,19 +6,19 @@ $heure = $_POST['heure'];
 $date = $_POST['date'];
 $paiement = $_POST['paiement'];
 $nombrePlaces = $_POST['nbr_place'];
-$ref_salle = $_POST['ref_salle'];
+
 $bdd = new PDO('mysql:host=localhost:3306;dbname=vsa_cinema;charset=utf8', 'root', '');
 
 //CODE
 
-    $requete = $bdd->prepare('INSERT INTO reservation (nom,heure,date,paiement,nbr_place,ref_salle) VALUES (:nom,:heure,:date,:paiement,:nbr_place,:ref_salle)');
+    $requete = $bdd->prepare('INSERT INTO reservation (nom,heure,date,paiement,nbr_place) VALUES (:nom,:heure,:date,:paiement,:nbr_place');
     $requete->execute(array(
         'nom' => $nom,
         'heure' => $heure,
         'date' => $date,
         'paiement' => $paiement,
         'nbr_place' => $nombrePlaces,
-        'ref_salle' => $salle,
+
     ));
-    header("Location: reussi res.html");
+    header("Location: reussi_res.html");
 
